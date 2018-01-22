ecf78d49c0f17abf21b7f56925f412d6612097f8
JaGgu Sam <iamjaggusam@gmail.com>
2018-01-22 11:31:06 +0530

    Initial Commit

---
+++ ByndrTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ByndrTest
+//
+//  Created by Jaggu Sam on 22/01/18.
+//  Copyright © 2018 Jaggu Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
